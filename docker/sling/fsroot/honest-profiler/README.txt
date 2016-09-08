cd34d55f28481baac9b1e5dfb79df2accdf26c7d
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-08 14:26:48 +0200

    Experimenting with honest-profiler, data goes under /sling-volume/profiler/<container ID>

---
+++ docker/sling/fsroot/honest-profiler/README.txt
@@ -0,0 +1,1 @@
+This folder contains the honest profiler distribution, from https://github.com/RichardWarburton/honest-profiler
--- docker/sling/slingroot/start.sh
+++ docker/sling/slingroot/start.sh
@@ -2,8 +2,15 @@
 export JAVA_OPTS="-Xmx512m"
 export SLING_OPTS="-Dsling.run.modes=oak_mongo,oak -Doak.mongo.uri=mongodb://mongo:27017 -Doak.mongo.db=${SLING_DB} -p ${SLING_PORT}"
 
+export PROF_DIR=/sling-volume/profiler/$(hostname)
+export PROF_FILE=$PROF_DIR/log.hpl
+mkdir -p $PROF_DIR
+export PROF="-agentpath:/honest-profiler/liblagent.so=interval=7,logPath=$PROF_FILE"
+echo Adding honest-profiler options: $PROF
+export JAVA_OPTS="$JAVA_OPTS $PROF"
+
 echo Starting Sling in `pwd`
-echo JAVA_OPTS=$JAVA_OPTS
+echo JAVA_OPTS="$JAVA_OPTS"
 echo SLING_OPTS=$SLING_OPTS
 
 /bin/bash ./announce.sh
