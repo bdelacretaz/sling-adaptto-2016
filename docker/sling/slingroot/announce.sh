81471da00b6ce443eba2c4b9884c24e6ba5175ce
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-12 11:45:45 +0200

    Rename 'domain' to 'role' in Sling instances setup

--- docker/sling/slingroot/announce.sh
+++ docker/sling/slingroot/announce.sh
@@ -7,4 +7,4 @@ export URL="http://etcd:4001/v2/keys/sling/instances/`hostname`"
 echo "Announcing $IP:$SLING_PORT to $URL"
 
 # TODO need json escapes
-curl -s ${URL} -XPUT -d value="{\"ip\":\"$IP\",\"port\":\"$SLING_PORT\",\"domain\":\"$SLING_DOMAIN\"}"
+curl -s ${URL} -XPUT -d value="{\"ip\":\"$IP\",\"port\":\"$SLING_PORT\",\"role\":\"$SLING_ROLE\"}"
