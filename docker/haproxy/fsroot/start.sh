2f3f5beb48f91fe31f78c8c122a636ee7d0972d8
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-12 18:00:25 +0200

    Log backend changes in haproxy image

--- docker/haproxy/fsroot/start.sh
+++ docker/haproxy/fsroot/start.sh
@@ -4,4 +4,16 @@ NODE=http://etcd:4001
 # WARNING make sure this is consistent between start.sh and reload.sh
 haproxy -D -f /etc/haproxy/haproxy.cfg -p /var/haproxy/haproxy.pid -sf $(cat /var/haproxy/haproxy.pid)
 
-/usr/local/bin/confd -interval=2 -verbose --config-file=/etc/confd/conf.d/haproxy.toml -node=$NODE
+# log changes to our backends config
+export CFG="/tmp/haproxy-tmp.cfg"
+> $CFG
+while true
+do
+  inotifywait -e close_write $CFG
+  echo "***** $CFG changed *****"
+  sleep 1
+  cat $CFG
+  echo "************************"
+done &
+
+/usr/local/bin/confd -interval=2 --config-file=/etc/confd/conf.d/haproxy.toml -node=$NODE
