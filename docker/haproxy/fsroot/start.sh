63da792b69bacd708da5c146948fa6781453e3b9
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-12 18:45:43 +0200

    Prepare for backend + frontend HTTP proxies

--- docker/haproxy/fsroot/start.sh
+++ docker/haproxy/fsroot/start.sh
@@ -4,6 +4,10 @@ NODE=http://etcd:4001
 # WARNING make sure this is consistent between start.sh and reload.sh
 haproxy -D -f /etc/haproxy/haproxy.cfg -p /var/haproxy/haproxy.pid -sf $(cat /var/haproxy/haproxy.pid)
 
+# save parameters for reload.sh
+echo SLING_ROLE_REGEXP="$SLING_ROLE_REGEXP"
+echo $SLING_ROLE_REGEXP > /tmp/sling-role-regexp.txt
+
 # log changes to our backends config
 export CFG="/tmp/haproxy-tmp.cfg"
 > $CFG
