bddb2de9f3e2fd26c8bb010109082a0b6631913c
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-26 14:03:08 +0200

    Demo scenario tweaks

--- scripts/1-setup-tmp-test.sh
+++ scripts/1-setup-tmp-test.sh
@@ -3,3 +3,8 @@ curl -Fsling:resourceType=test http://${H}/tmp/test
 curl -X POST http://${H}/cluster/routing/scripts/test
 export W=fileserver
 echo $W > /tmp/1 && curl -T /tmp/1 http://${H}/cluster/routing/scripts/test/json.routing
+
+echo
+echo "********************************************************************"
+echo "Contents of http://${H}/cluster/routing/scripts/test/json.routing :"
+curl -s http://${H}/cluster/routing/scripts/test/json.routing
