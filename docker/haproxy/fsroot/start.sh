fe734cb728bb3196b65d4b13e44b122a08d0c47c
Bertrand Delacretaz <bdelacretaz@apache.org>
2016-09-27 16:49:43 +0200

    Workaround for difficulties finding etcd in containers

--- docker/haproxy/fsroot/start.sh
+++ docker/haproxy/fsroot/start.sh
@@ -12,5 +12,9 @@ cat /tmp/SLING_ENV.sh
 # log changes of interesting files
 ./logchanges.sh /etc/haproxy/haproxy.cfg
 
+# hack: add an entry for etcd to our hosts file
+echo "$(ping etcd -c 1 | head -1 | cut -d'(' -f2 | cut -d')' -f1)" etcd >> /etc/hosts
+cat /etc/hosts
+
 # confd watches etcd and triggers reload.sh upon changes
 /usr/local/bin/confd -interval=2 --config-file=/etc/confd/conf.d/haproxy.toml -node=$NODE
